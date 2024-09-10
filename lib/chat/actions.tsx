fce8f457fe9a9c1268a924277e81217ebb50206a
Daun <iam@daun.io>
2024-09-11 03:35:59 +0900

    fix function names

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -52,10 +52,10 @@ Messages inside [] means that it's a UI element or a user event. For example:
 - "[Price of AAPL = 100]" means that an interface of the stock price of AAPL is shown to the user.
 - "[User has changed the amount of AAPL to 10]" means that the user has changed the amount of AAPL to 10 in the UI.
 
-If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
-If the user just wants the price, call \`show_stock_price\` to show the price.
-If you want to show trending stocks, call \`list_stocks\`.
-If you want to show events, call \`get_events\`.
+If the user requests purchasing a stock, call \`showStockPurchaseUI\` to show the purchase UI.
+If the user just wants the price, call \`showStockPrice\` to show the price.
+If you want to show trending stocks, call \`listStocks\`.
+If you want to show events, call \`getEvents\`.
 If the user wants to sell stock, or complete another impossible task, respond that you are a demo and cannot do that.
 
 Besides that, you can also chat with users and do some calculations if needed.`
